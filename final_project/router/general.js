e885a4098995d68e10b3e0509e0b0ba45f40e29b
Connor Frunzi <connor.frunzi@icloud.com>
2025-08-10 12:51:58 -0400

    added Promise callbacks for ISBN and book list

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,20 +10,53 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+// Task 10
+// retrieve the book list using Promise callback
+const getBookList = () => {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+};
+
 // Get the book list available in the shop
 // Task 1
-public_users.get('/', function (req, res) {
-  //Write your code here
+public_users.get('/', async function (req, res) {
+  try{
+    const books = await getBookList();
+    res.json(books);
+  } 
+  catch(e){
+    console.error(e);
+    res.status(500).json({message: "Error retrieving the book list"})
+  }
   res.send(JSON.stringify(books, null, 2));
 });
 
+// Task 11
+// retrieve ISBN details based on Promise Callback
+const getISBN = () => {
+    return new Promise((resolve, reject) => {
+        let isbn = parseInt(isbn);
+
+        if(books[isbn]){
+            resolve(books[isbn]);
+        } else{
+            reject({status: 404, message: `ISBN ${isbn} not found`});
+        }
+    });
+};
+
 // Task 2
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', async function (req, res) {
+    getISBN(req.params.isbn)
+    .then(
+        result => res.send(result),
+        error => res.status(error.status).json({message: error.message})
+    );
+});
   
+// Task 3
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
