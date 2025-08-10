3b603a0ac288903595118fa0d191595ef3044407
Connor Frunzi <connor.frunzi@icloud.com>
2025-08-10 13:20:00 -0400

    bug fix: handle NaN values when handling promise request

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,33 +34,34 @@ public_users.get('/', async function (req, res) {
 
 // Task 11
 // retrieve ISBN details based on Promise Callback
-const getISBN = () => {
+const getISBN = (isbn) => {
     return new Promise((resolve, reject) => {
-        let isbn = parseInt(isbn);
+        let parsedISBN = parseInt(isbn);
 
-        if(books[isbn]){
-            resolve(books[isbn]);
+        if(books[parsedISBN]){
+            resolve(books[parsedISBN]);
         } else{
-            reject({status: 404, message: `ISBN ${isbn} not found`});
+            reject({status: 404, message: `ISBN ${parsedISBN} not found`});
         }
     });
 };
 
 // Task 2
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', async function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
     getISBN(req.params.isbn)
-    .then(
-        result => res.send(result),
-        error => res.status(error.status).json({message: error.message})
-    );
+    .then(book => {
+        res.send(JSON.stringify(book, null, 2));
+    })
+    .catch(err => {
+        res.status(err.status || 500).json({error: err.message || "Internal Server Error"});
+    });
 });
   
 // Task 3
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
 });
 
 // Get all books based on title
