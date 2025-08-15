1b8a072fa90f9fba77034b244f17b2de551b7772
Connor Frunzi <connor.frunzi@icloud.com>
2025-08-15 13:38:59 -0400

    added authentication functionality for existing users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,36 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+// Task 7
+const isValid = (username)=>{ 
+    // returns boolean
+    // confirm user already exists 
+    const userMatch = users.filter((user) => user.username === username);
+    return userMatch.length > 0;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+// only allow registered users to login
+const authenticatedUser = (username,password)=>{ 
+    //returns boolean
+    const existingUsers = users.filter((user) => user.username === username && user.password === password);
+    return existingUsers.length > 0;
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (authenticatedUser(username, password)){
+        // allow 30 minute session for authentication
+        let bearerToken = jwt.sign({data:password}, "access", {"expiresIn": 1800});
+        req.session.authentication = {bearerToken, username};
+        return res.status(200).json({message: "User successfully logged in"});
+    } else {
+        return res.status(401).json({message: "Invalid username or password"});
+    }
 });
 
 // Add a book review
